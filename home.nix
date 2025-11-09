ae437e6dd6533bebb2d165a1c3fcd429b6ccbc86
21givenchy <65071041+21givenchy@users.noreply.github.com>
2025-11-09 17:53:49 +0300

    Create home.nix

---
+++ home.nix
@@ -0,0 +1,53 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+ #    ./modules/hyprland.nix
+    ./modules/waybar.nix
+    ./modules/themes.nix
+    ./modules/programs.nix
+  ];
+
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+  "google-chrome"
+  "microsoft-edge"
+];
+  # Basic packages
+  home.packages = with pkgs; [
+    # Core utilities
+    mat2
+    protonvpn-gui
+    tree
+    curl
+    wget
+    firefox
+    fastfetch
+    spotify
+    telegram-desktop
+    microsoft-edge
+       nodejs
+    git
+    neovim
+    docker
+    alacritty
+    google-chrome
+    chromium
+    unzip
+    discord
+    # Media
+    mpv
+    kdePackages.dolphin
+    # System tools
+    dunst
+
+      wofi
+   # hyprpaper
+    #hyprland-protocols
+    #grimblast
+  ];
+
+  programs.gurk-rs.enable = lib.mkDefault false;
+
+  home.stateVersion = "25.11";
+}
